72a2abe45370c3ca61ce43f5c1cfdadaad438d0c
3omarbadr <omarbadr167@gmail.com>
2022-04-09 16:30:19 +0200

    create invoice module

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,7 @@ use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\AdminController;
+use App\Http\Controllers\InvoiceController;
 
 /*
 |--------------------------------------------------------------------------
@@ -25,5 +26,7 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', [HomeController::class, 'index'])->name('home');
+
+Route::resource('invoices', InvoiceController::class);
 
 Route::get('/{page}', [AdminController::class,'index']);
